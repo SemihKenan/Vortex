245570a2845afe92c9014bbddb07f949e9f9ba70
Semih Kenan Turgut <semihkenan12@gmail.com>
2025-12-12 18:43:57 +0300

    Upload project files to github

---
+++ composeApp/src/jvmMain/kotlin/com/vortex/app/main.kt
@@ -0,0 +1,13 @@
+package com.vortex.app
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Vortex",
+    ) {
+        App()
+    }
+}
